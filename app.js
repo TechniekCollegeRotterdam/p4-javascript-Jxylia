ab5ac35ef39df4a2dd757b516966446c84dbfa30
Jxylia <70641347+Jxylia@users.noreply.github.com>
2022-06-03 13:13:16 +0200

    opdracht 3

--- app.js
+++ app.js
@@ -24,20 +24,24 @@ window.addEventListener("keydown", function(e){
     if(e.key == "ArrowRight"){
         px += 50;
         bird.style.left = px + "px";
+        bird.src = "img/b-right.svg";
     }
 
     if(e.key == "ArrowUp"){
         px += 50;
         bird.style.bottom = px + "px";
+        bird.src = "img/b-down.svg";
     }
 
     if(e.key == "ArrowDown"){
         px += 50;
         bird.style.top = px + "px";
+        bird.src = "img/b-up.svg";
     }
 
     if(e.key == "ArrowLeft"){
         px += 50;
         bird.style.right = px + "px";
+        bird.src = "img/b-left.svg";
     }
 })
